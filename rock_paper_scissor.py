c879158dbff95fc1cade4be5628fb36c18a8e1a9
celine <89533499+celineai@users.noreply.github.com>
2025-06-20 13:07:44 +0300

    Update rock_paper_scissor.py
    Refactored the code into easier functions and helps maintain the readibility.

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -3,17 +3,22 @@ import random
 emojis = {'r': '🪨', 'p': '📄', 's': '✂️'}
 choices = ('r', 'p', 's')
 
-while True:
-    user_choice = input("Rock, paper, or scissors? (r/p/s): ").lower()
-    if user_choice not in choices:
-        print("Invalid choice!")
-        continue
 
-    computer_choice = random.choice(choices)
+def get_user_choice():
+    while True:
+        user_choice = input("Rock, paper, or scissors? (r/p/s): ").lower()
+        if user_choice in choices:
+            return user_choice
+        else:
+            print("Invalid choice!")
 
+
+def display_choices(user_choice, computer_choice):
     print(f"You chose {emojis[user_choice]}")
     print(f"Compute chose {emojis[computer_choice]}")
 
+
+def determine_winner(user_choice, computer_choice):
     if user_choice == computer_choice:
         print('Tie!')
     elif (
@@ -24,6 +29,20 @@ while True:
     else:
         print("You lose")
 
-    should_continue = input('Continue? (y/n): ').lower()
-    if should_continue == 'n':
-        break
+
+def play_grame():
+    while True:
+        user_choice = get_user_choice()
+
+        computer_choice = random.choice(choices)
+
+        display_choices(user_choice, computer_choice)
+
+        determine_winner(user_choice, computer_choice)
+
+        should_continue = input('Continue? (y/n): ').lower()
+        if should_continue == 'n':
+            break
+
+
+play_game()
